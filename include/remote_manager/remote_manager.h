47a800933317e301d2b6205d2b86f139da1f38e5
SoshiroMaruoka <r119163@g.kobe-kosen.ac.jp>
2024-11-24 00:50:22 +0000

    add pause function

--- include/remote_manager/remote_manager.h
+++ include/remote_manager/remote_manager.h
@@ -2,11 +2,15 @@
 #define REMOTE_MANAGER_H
 
 #include <ros/ros.h>
+#include <time.h>
 
 #include <geometry_msgs/Twist.h>
 
 #include <std_msgs/Int32.h>
 #include <std_msgs/Bool.h>
+
+#include <waypoint_msgs/waypoint.h>
+#include <waypoint_msgs/waypoints.h>
 
 class RemoteManager
 {
@@ -17,19 +21,24 @@ class RemoteManager
 
         void Drive(const geometry_msgs::Twist& cmd_vel_used);
         void Stop();
+        void Pause();
         // void Accelerate();
         // void DecelerateStop();
 
+    private:
         void cmd_vel_ros_cb(const geometry_msgs::Twist& ros_msg);
         void cmd_vel_remote_cb(const geometry_msgs::Twist& remote_msg);
         void emg_switch_cb(const std_msgs::Bool& emg_msg);
         void mode_switch_cb(const std_msgs::Int32& mode_msg);
+        void waypoints_cb(waypoint_msgs::waypointsConstPtr msg);
+        void waypoint_cb(waypoint_msgs::waypointConstPtr msg);
 
-    private:
         ros::Subscriber _cmd_vel_ros_sub;
         ros::Subscriber _cmd_vel_remote_sub;
         ros::Subscriber _emg_switch_sub;
         ros::Subscriber _mode_switch_sub;
+        ros::Subscriber _wp_sub;
+        ros::Subscriber _wps_sub;
         ros::Publisher _cmd_vel_pub;
 
         geometry_msgs::Twist _cmd_vel_ros;
@@ -39,6 +48,11 @@ class RemoteManager
         geometry_msgs::Twist _cmd_vel;
 
         const geometry_msgs::Twist cmd_vel_zero;
+
+        int _wpIndex_now;
+        waypoint_msgs::waypoints _wps;
+        waypoint_msgs::waypoint_attribute attribute;
+        bool pause_done;
 
 };
 
--- src/remote_manager.cpp
+++ src/remote_manager.cpp
@@ -2,18 +2,22 @@
 
 RemoteManager::RemoteManager(ros::NodeHandle &nh, ros::NodeHandle &pn)
 {
-    std::string cmd_vel_ros, cmd_vel_remote, emg_switch, mode_switch, cmd_vel;
+    std::string cmd_vel_ros, cmd_vel_remote, emg_switch, mode_switch, cmd_vel, topic_waypoints, topic_waypoint;
     
     pn.param<std::string>("cmd_vel_ros", cmd_vel_ros, "/naviton/mobile_base_controller/cmd_vel/ros");
     pn.param<std::string>("cmd_vel_remote", cmd_vel_remote, "/mqtt/remote_controller/cmd_vel");
     pn.param<std::string>("emg_switch", emg_switch, "/mqtt/remote_controller/emg_switch");
     pn.param<std::string>("mode_switch", mode_switch, "/mqtt/remote_controller/mode_switch");
+    pn.param<std::string>("topic_waypoint", topic_waypoint, "/waypoint_manager/pManager/nowWp");
+    pn.param<std::string>("topic_waypoints", topic_waypoints, "/waypoint_manager/wpLoader/waypoints");
     pn.param<std::string>("cmd_vel", cmd_vel, "/naviton/mobile_base_controller/cmd_vel");
 
     _cmd_vel_ros_sub = nh.subscribe(cmd_vel_ros, 10, &RemoteManager::cmd_vel_ros_cb, this);
     _cmd_vel_remote_sub = nh.subscribe(cmd_vel_remote, 10, &RemoteManager::cmd_vel_remote_cb, this);
     _emg_switch_sub = nh.subscribe(emg_switch, 10, &RemoteManager::emg_switch_cb, this);
     _mode_switch_sub = nh.subscribe(mode_switch, 10, &RemoteManager::mode_switch_cb, this);
+    _wp_sub = nh.subscribe(topic_waypoint, 1, &RemoteManager::waypoint_cb, this);
+    _wps_sub = nh.subscribe(topic_waypoints, 1, &RemoteManager::waypoints_cb, this);
     _cmd_vel_pub = nh.advertise<geometry_msgs::Twist>(cmd_vel, 10);
 }
 
@@ -21,10 +25,20 @@ void RemoteManager::init()
 {
     _emg_switch = 0;
     _mode_switch = 0;
+    pause_done = true;
 }
 
 void RemoteManager::update()
 {
+    if(_wps.waypoints[_wpIndex_now].attributes[1].type == 1)
+    {
+        _mode_switch = 3;
+        pause_done = false;
+    }
+    else
+    {
+        pause_done = true;
+    }
     if(_emg_switch){_mode_switch = 2;}
     {
         switch(_mode_switch)
@@ -37,6 +51,9 @@ void RemoteManager::update()
                 break;
             case 2:
                 Stop();
+                break;
+            case 3:
+                pause();
                 break;
             default:
                 return;
@@ -55,19 +72,37 @@ void RemoteManager::Stop()
     _cmd_vel = cmd_vel_zero;
 }
 
+void RemoteManager::Pause()
+{
+    if(!pause_done)
+    {
+        ros::WallTime wall_begin = ros::WallTime::now();
+        ros::WallTime wall_now = ros::WallTime::now();
+        ros::WallDuration wall_duration = wall_now - wall_begin;
+        while(wall_duration.sec < _wps.waypoints[_wpIndex_now].attributes[1].value)
+        {
+            Stop();
+            wall_now = ros::WallTime::now();
+            wall_duration = wall_now - wall_begin;
+        }
+    }
+    else
+    {
+        Drive(_cmd_vel_ros);
+    }
+    pause_done = true;
+}
 // void RemoteManager::Accelerate(const geometry_msgs::Twist& cmd_vel_target, double max_linear_accelerate, double max_angular_accelerate)
 // {
+//     ros::WallTime accel_now = ros::WallTime::now();
+//     ros::WallDuration accel_duration = accel_now - accel_last;
+//     accel_last = accel_now;
 //     unsigned long time_now = micros();
 //     double _dt = (time_now - _time_last);
 //     _time_last = time_now;
 
-//     _linear_velocity += constrain((linear_velocity - _linear_velocity)*1e6/_dt, -max_linear_accelerate, max_linear_accelerate) * _dt / 1e6;
-//     _angular_velocity += constrain((angular_velocity - _angular_velocity)*1e6/_dt, -max_angular_accelerate, max_angular_accelerate) * _dt / 1e6;
-
-//     _linear_velocity = constrain(_linear_velocity, -1.0, 1.0);
-//     _angular_velocity = constrain(_angular_velocity, -1.0, 1.0);
-
-//     Drive(_linear_velocity, _angular_velocity);
+//     _cmd_vel.linear.x += constrain((cmd_vel_target.linear.x - _cmd_vel.linear.x) * 1000000000 / accel_duration.nsec, -max_linear_accelerate, max_linear_accelerate) * accel_duration.nsec / 1000000000;
+//     _cmd_vel.angular.z += constrain((cmd_vel_target.angular.z - _cmd_vel.angular.z) * 1000000000 / accel_duration.nsec, max_angular_accelerate) * accel_duration.nsec / 1000000000;
 // }
 
 // void RemoteManager::DecelerateStop()
@@ -82,7 +117,8 @@ void RemoteManager::cmd_vel_ros_cb(const geometry_msgs::Twist& ros_msg)
 
 void RemoteManager::cmd_vel_remote_cb(const geometry_msgs::Twist& remote_msg)
 {
-    _cmd_vel_remote = remote_msg;
+    _cmd_vel_remote.linear.x = 0.4 * remote_msg.linear.x;
+    _cmd_vel_remote.angular.z = 0.3 * remote_msg.angular.z;
 }
 
 void RemoteManager::emg_switch_cb(const std_msgs::Bool& emg_msg)
@@ -93,4 +129,14 @@ void RemoteManager::emg_switch_cb(const std_msgs::Bool& emg_msg)
 void RemoteManager::mode_switch_cb(const std_msgs::Int32& mode_msg)
 {
     _mode_switch = mode_msg.data;
+}
+
+void RemoteManager::waypoint_cb(waypoint_msgs::waypointConstPtr msg)
+{
+    _wpIndex_now = msg->index;
+}
+
+void RemoteManager::waypoints_cb(waypoint_msgs::waypointsConstPtr msg)
+{
+    _wps = *msg;
 }
